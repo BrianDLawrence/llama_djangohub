a720a227f54201f70d26ab9d30074c2179ca9082
Brian Lawrence <Brian@BrianDLawrence.com>
2024-02-29 17:20:22 +0100

    Minor changes to get deployment to AWS successfull

--- hub/settings.py
+++ hub/settings.py
@@ -21,10 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = get_env_variable('HUB_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
+DEBUG = os.getenv('DJANGO_DEBUG', 'False') == 'True'
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost').split(',')
 
 # Application definition
 
