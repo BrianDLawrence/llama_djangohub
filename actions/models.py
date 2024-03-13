b4902d9da1e0f19da10f457e6777ab681642f021
Brian Lawrence <Brian@BrianDLawrence.com>
2024-03-13 18:27:30 +0100

    Updating model, removed prompts, added context

--- actions/models.py
+++ actions/models.py
@@ -2,22 +2,19 @@ from django.db import models
 #pylint: disable=invalid-str-returned
 #pylint doesn't think CharField is str
 
+class Context(models.Model):
+    name = models.CharField(max_length=256)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
 class Actions(models.Model):
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=256)
     description = models.CharField(max_length=1024, blank=True, null=True)
     success_criteria = models.TextField(default="")
     prompt = models.TextField(default="")
     created = models.DateTimeField(auto_now_add=True)
-    def __str__ (self):
-        return self.name
-
-class Prompts(models.Model):
-    name = models.CharField(max_length=200)
-    description = models.CharField(max_length=1024)
-    action = models.ForeignKey(
-        Actions,
-        on_delete=models.CASCADE,
-        related_name='prompts',
-    )
+    context = models.ForeignKey(Context, on_delete=models.CASCADE, related_name='actions', null=True, blank=True)
     def __str__ (self):
         return self.name
--- actions/tests/test_models.py
+++ actions/tests/test_models.py
@@ -1,9 +1,9 @@
 from django.test import TestCase
 from django.core.exceptions import ValidationError
-from ..models import Actions, Prompts
+from ..models import Actions, Context
 # pylint: disable=no-member
 
-class ActionsModelTest(TestCase):
+class ActionsModelTestNoContext(TestCase):
     def test_field_content(self):
         action = Actions.objects.create(name="Test Action", description="Test Description",
                                         prompt="Test Prompt", success_criteria="Test Success Criteria")
@@ -14,28 +14,39 @@ class ActionsModelTest(TestCase):
         self.assertIsNotNone(action.created)
         self.assertTrue(isinstance(action, Actions))
 
-class PromptsModelTest(TestCase):
+class ActionsModelTestWithContext(TestCase):
     def test_field_content(self):
+        context = Context.objects.create(name="Test Context",
+                                         description="Context Description")
+        action = Actions.objects.create(name="Test Action", description="Test Description",
+                                        prompt="Test Prompt", success_criteria="Test Success Criteria",
+                                        context = context)
+        self.assertEqual(action.name, "Test Action")
+        self.assertEqual(action.description, "Test Description")
+        self.assertEqual(action.success_criteria, "Test Success Criteria")
+        self.assertEqual(action.prompt, "Test Prompt")
+        self.assertEqual(action.context, context)
+        self.assertIsNotNone(action.created)
+        self.assertTrue(isinstance(action, Actions))
+
+class ActionsModelTestAddContextLater(TestCase):
+    def test_field_content(self):
+        context = Context.objects.create(name="Test Context",
+                                         description="Context Description")
         action = Actions.objects.create(name="Test Action", description="Test Description",
                                         prompt="Test Prompt", success_criteria="Test Success Criteria")
-        prompt = Prompts.objects.create(name="Test Prompt",
-                                         description="Prompt Description", action=action)
+        self.assertEqual(action.name, "Test Action")
+        self.assertEqual(action.description, "Test Description")
+        self.assertEqual(action.success_criteria, "Test Success Criteria")
+        self.assertEqual(action.prompt, "Test Prompt")
+        self.assertIsNotNone(action.created)
+        self.assertTrue(isinstance(action, Actions))
 
-        self.assertEqual(prompt.name, "Test Prompt")
-        self.assertEqual(prompt.description, "Prompt Description")
-        self.assertEqual(prompt.action, action)
-        self.assertTrue(isinstance(prompt, Prompts))
+        action.context = context
+        action.save()
 
-class ModelRelationshipTest(TestCase):
-    def test_prompts_in_actions(self):
-        action = Actions.objects.create(name="Action", description="Action Description",
-                                        prompt="Action Prompt", success_criteria="Test Success Criteria")
-        Prompts.objects.create(name="Prompt 1",
-                               description="Description 1", action=action)
-        Prompts.objects.create(name="Prompt 2",
-                               description="Description 2", action=action)
-
-        self.assertEqual(action.prompts.count(), 2)
+        updated_action = Actions.objects.get(id=action.id)
+        self.assertEqual(updated_action.context, context)
 
 class ActionsModelValidationTest(TestCase):
     def test_create_invalid_if_name_empty(self):
@@ -48,9 +59,11 @@ class ActionsModelStrTest(TestCase):
         action = Actions.objects.create(name="Action Name")
         self.assertEqual(str(action), "Action Name")
 
-class PromptsModelStrTest(TestCase):
-    def test_str_representation(self):
-        action = Actions.objects.create(name="Action Name")
-        prompt = Prompts.objects.create(name="Test Prompt",
-                                         description="Prompt Description", action=action)
-        self.assertEqual(str(prompt), "Test Prompt")
+class ContextsModelTest(TestCase):
+    def test_field_content(self):
+        context = Context.objects.create(name="Test Context",
+                                         description="Context Description")
+
+        self.assertEqual(context.name, "Test Context")
+        self.assertEqual(context.description, "Context Description")
+        self.assertTrue(isinstance(context, Context))
